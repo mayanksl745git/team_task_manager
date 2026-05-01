33ee900f23976fc53f494cef1da7a701bb3d1e5f
Mayank Singhal <mayanksinghal.2004@gmail.com>
2026-05-02 03:06:10 +0530

    serve frontend from express

--- server/server.js
+++ server/server.js
@@ -38,12 +38,11 @@ app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    data: {},
-    message: `Route ${req.originalUrl} not found`,
-  });
+// Serve React frontend
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.use((error, _req, res, _next) => {
