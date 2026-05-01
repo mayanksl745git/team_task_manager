0c87e630c82fb50bebe4f9bc86e09495acffde7b
Mayank Singhal <mayanksinghal.2004@gmail.com>
2026-05-02 03:12:45 +0530

    fix express 5 wildcard route

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use('/api/tasks', taskRoutes);
 // Serve React frontend
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (_req, res) => {
+app.get(/(.*)/, (_req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
